0e48633b8d96bd2e662fdcd8c0fcaa6054dee8d4
SEV AI Solutions <info@sevai.com.au>
2026-01-07 02:24:46 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,26 @@ import Proof from "../components/sections/Proof";
 import HowItWorks from "../components/sections/HowItWorks";
 import HowWeWork from "../components/sections/HowWeWork";
 import CTA from "../components/sections/CTA";
+import SectionDivider from "../components/SectionDivider";
 
 export default function HomePage() {
   return (
     <main className="relative">
       <Hero />
+
+      <SectionDivider />
       <StatsStrip />
+
+      <SectionDivider />
       <Proof />
+
+      <SectionDivider />
       <HowItWorks />
+
+      <SectionDivider />
       <HowWeWork />
+
+      <SectionDivider />
       <CTA />
     </main>
   );
