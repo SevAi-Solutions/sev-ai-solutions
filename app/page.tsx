6ed615e3fe57df2fb751616c751b1677930248a1
SEV AI Solutions <info@sevai.com.au>
2026-01-07 00:20:41 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "../components/sections/Hero";
+import StatsStrip from "../components/sections/StatsStrip";
 
 export default function HomePage() {
   return (
     <main className="relative">
       <Hero />
+      <StatsStrip />
+      {/* Next: add Proof / HowItWorks / HowWeWork / CTA */}
     </main>
   );
 }
